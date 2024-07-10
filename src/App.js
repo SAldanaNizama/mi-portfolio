2847bd4d9ac82bef4b0c0a2540478e745bfdfde2
SAldanaNizama <140188751+SAldanaNizama@users.noreply.github.com>
2024-07-10 15:25:55 -0500

    delete footer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import {Route, Routes } from 'react-router-dom';
-import Home from './pages/home/Home.jsx';
-import Navbar from "./components/navbar/Navbar.jsx"
-import Projects from "./pages/features/Projects.jsx"
-import Skills from './pages/pricing/Skills.jsx';
-import About from "./pages/about/About.jsx"
-import Footer from './components/footer/Footer.jsx';
-import CustomCursor from './components/customCursor/CustomCursor.jsx';
-import "./App.css"
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home.jsx";
+import Navbar from "./components/navbar/Navbar.jsx";
+import Projects from "./pages/features/Projects.jsx";
+import Skills from "./pages/pricing/Skills.jsx";
+import About from "./pages/about/About.jsx";
+import CustomCursor from "./components/customCursor/CustomCursor.jsx";
+import "./App.css";
 
 const App = () => {
   return (
     <div className=" bg-black min-h-screen">
-     <CustomCursor/>
-      <Navbar/>
+      <CustomCursor />
+      <Navbar />
       <Routes>
         <Route path="/projects" element={<Projects />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/about" element={<About />} />
         <Route path="/" element={<Home />} />
       </Routes>
-      <Footer/>
     </div>
   );
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./home.css";
+
 const Home = () => {
   return (
     <header className="relative py-8 grid grid-cols-1 md:grid-cols-2 gap-8 items-center max-w-screen-xl mx-auto">
@@ -18,6 +19,32 @@ const Home = () => {
           and am committed to continuous learning. My goal is to become a senior
           developer while sharing my passion for programming with others.
         </p>
+        <div className="socials flex items-center gap-4">
+          <a
+            href="https://www.linkedin.com/in/samir-aldana-nizama-579587260/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 border border-gray-300 p-2 text-xl rounded-full transition-all duration-300 hover:text-white hover:bg-blue-600 hover:border-blue-600"
+          >
+            <i className="ri-linkedin-fill"></i>
+          </a>
+          <a
+            href="https://wa.me/916436814"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 border border-gray-300 p-2 text-xl rounded-full transition-all duration-300 hover:text-white hover:bg-blue-600 hover:border-blue-600"
+          >
+            <i className="ri-whatsapp-line"></i>
+          </a>
+          <a
+            href="mailto:samirnizama13@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 border border-gray-300 p-2 text-xl rounded-full transition-all duration-300 hover:text-white hover:bg-blue-600 hover:border-blue-600"
+          >
+            <i className="ri-mail-line"></i>
+          </a>
+        </div>
       </div>
       <div className="image relative">
         <img
